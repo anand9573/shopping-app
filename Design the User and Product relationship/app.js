692321fe8ee09b435931d28437040778353bcaba
Anand Naik <anandbukyanaik9573@gmail.com>
2023-08-14 18:15:12 +0530

    Cart with many to many relationship

--- Design the User and Product relationship/app.js
+++ Design the User and Product relationship/app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product=require('./models/product');
 const User=require('./models/user');
+const Cart=require('./models/cart');
+const CartItem=require('./models/cart-item');
 
 const app = express();
 
@@ -15,7 +17,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const { userInfo } = require('os');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,6 +35,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through:CartItem});
+Product.belongsToMany(Cart,{through:CartItem});
 
 sequelize
   .sync()
@@ -46,6 +51,8 @@ if(!user){
   return user;
 }).then(user=>{
   // console.log(user);
+  return user.createCart();
+}).then(cart=>{
   app.listen(3000);
 })
 .catch(err => {
